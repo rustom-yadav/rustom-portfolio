8d1ce9dae68aac5e6c31493df0a7f2f8613a044f
Rustom Yadav <vijayyadav.official@outlook.com>
2026-09-15 17:48:54 +0530

    feat: add Hero and SkillCard components with react-icons dependency

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -23,6 +23,7 @@ function TypewriterRole() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
